5103a70224f7c7c70dd8f6ac646524c0da3a4131
Gabriel Lee <gabriel.lee@credera.co.uk>
2022-11-25 14:10:37 +0000

    feat: export all needed types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { MapCanvas } from './map-canvas'
-import { ColorDef, ColorDefs, LineDef, TransitProps } from './types'
+import { ColorDef, ColorDefs, LineDef, TransitProps, Station, TransitPath } from './types'
 
 export default MapCanvas;
-export type { ColorDef, ColorDefs, LineDef, TransitProps };
+export type { ColorDef, ColorDefs, LineDef, TransitProps, Station, TransitPath };
